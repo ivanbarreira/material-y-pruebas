d5b9b4ca664ac1d72064016498c29349496c1628
ivanbarreira <ivanchooh@gmail.com>
2019-03-25 12:23:52 -0300

    falta terminarlo bien funciones

--- main.c
+++ main.c
@@ -5,23 +5,14 @@ int obtenerMaximos(int x, int y, int z);
 
 int main()
 {
-    int x;
-    int y;
-    int z;
-    int numMax;
+    int x=15;
+    int y=16;
+    int z=14;
 
-    printf("ingrese el primer numero:");
-    scanf("%d", &x);
 
-    printf("ingrese el segundo numero:");
-    scanf("%d", &y);
+    printf("numMax"),obtenerMaximos(x, y, z);
 
-    printf("ingrese el tercer numero:");
-    scanf("%d\n", &z);
 
-    numMax = obtenerMaximos(x, y, z);
-
-    printf("El numero maximo es %d",numMax);
 
     return 0;
 }
@@ -34,9 +25,7 @@ int obtenerMaximos(int x, int y, int z)
     {
         numMax=x;
     }
-    else
-    {
-        if(y>x && y>z)
+        else if(y>x && y>z)
         {
             numMax=y;
         }
@@ -45,4 +34,5 @@ int obtenerMaximos(int x, int y, int z)
             numMax=z;
         }
     }
+    return numMax;
 }
